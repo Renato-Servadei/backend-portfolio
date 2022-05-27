a5517473fc90c51543de90f46813ee4615503fad
Rober <boblawyer1@gmail.com>
2022-05-27 12:21:29 -0300

    se arregló un problema en el servicio de habilidades duras

--- src/main/java/com/backend/PortfolioRS/service/DurasService.java
+++ src/main/java/com/backend/PortfolioRS/service/DurasService.java
@@ -1,13 +1,43 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.backend.PortfolioRS.service;
 
-/**
- *
- * @author rober
- */
-public class DurasService {
+import com.backend.PortfolioRS.interfaces.IDuras;
+import com.backend.PortfolioRS.model.Duras;
+import com.backend.PortfolioRS.repository.DurasRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DurasService implements IDuras{
+
+    @Autowired
+    public DurasRepository durasRepo;
+    
+    @Override
+    public List<Duras> verDuras() {
+    return durasRepo.findAll();
+    }
+
+    @Override
+    public void crearDuras(Duras duras) {
+       durasRepo.save(duras);
+    }
+
+    @Override
+    public void borrarDuras(Long idDuras) {
+        durasRepo.deleteById(idDuras);
+    }
+
+    @Override
+    public Duras buscarDuras(Long idDuras) {
+        return durasRepo.findById(idDuras).orElse(null);
+    }
+
+    @Override
+    public Duras editarDuras(Duras duras) {
+        return durasRepo.save(duras);
+    }
+    
     
 }
